36f0354a6fb5afd0624301d84f02bad27339bb73
Henrick Adrian <henrick.adrian4@gmail.com>
2026-05-26 19:56:55 -0300

    Configurações teste de integração.

--- 02 - TDD/src/NerdStore.Vendas.Data/VendasContext.cs
+++ 02 - TDD/src/NerdStore.Vendas.Data/VendasContext.cs
@@ -1,49 +1,67 @@
-﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using NerdStore.Core.Data;
-using NerdStore.Core.DomainObjects;
+using NerdStore.Core.Messages;
+using NerdStore.Vendas.Domain;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
 
-namespace NerdStore.Vendas.Data;
-
-public class VendasContext : DbContext, IUnitOfWork
+namespace NerdStore.Vendas.Data
 {
-    private readonly IMediator _mediator;
-
-    public VendasContext(DbContextOptions<VendasContext> options, IMediator mediator) : base(options)
+    public class VendasContext : DbContext, IUnitOfWork
     {
-        _mediator = mediator;
-    }
+        private readonly IMediator _mediatorHandler;
 
-    public async Task<bool> Commit()
-    {
-        var sucesso = await base.SaveChangesAsync() > 0;
 
-        if (sucesso) await _mediator.PublicarEventos(this);
+        public VendasContext(DbContextOptions<VendasContext> options, IMediator mediatorHandler)
+            : base(options)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
 
-        return sucesso;
-    }
-}
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoItem> PedidoItems { get; set; }
+        public DbSet<Voucher> Vouchers { get; set; }
 
-public static class MediatorExtension
-{
-    public static async Task PublicarEventos(this IMediator mediator, VendasContext ctx)
-    {
-        var domainEntities = ctx.ChangeTracker
-            .Entries<Entity>()
-            .Where(x => x.Entity.Notificacoes != null && x.Entity.Notificacoes.Any());
 
-        var domainEvents = domainEntities
-            .SelectMany(x => x.Entity.Notificacoes)
-            .ToList();
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
 
-        domainEntities.ToList()
-            .ForEach(entity => entity.Entity.LimparEventos());
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
 
-        var tasks = domainEvents
-            .Select(async (domainEvent) => {
-                await mediator.Publish(domainEvent);
-            });
+            var sucesso = await base.SaveChangesAsync() > 0;
+            if (sucesso) await _mediatorHandler.PublicarEventos(this);
 
-        await Task.WhenAll(tasks);
+            return sucesso;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(VendasContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.HasSequence<int>("MinhaSequencia").StartsAt(1000).IncrementsBy(1);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
